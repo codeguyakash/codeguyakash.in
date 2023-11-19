289ad3492cf082fcf61b4e74cc8cfc7e1ed6a507
Akash <codeguyakash@gmail.com>
2023-11-20 00:54:53 +0530

    api integrate

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,15 @@ import './ProjectCard.css'
 const projectCard = ({ items }) => {
   return (
     <div className='card-container'>
-      <div className='cards' style={{ background: items }}></div>
+      <div className='cards' style={{ background: items.color }}>
+        <img src="https://cdn-icons-png.flaticon.com/512/10832/10832132.png" alt='project-1' id='project-image' />
+       
+          <h3 className='color'>{items.title}</h3>
+          <p className='color' >{items.description}</p>
+          <a className='color' href={items.sourceCodeLink}>Source Code</a> <br />
+          <a className='color' href={items.demoLink}>Demo Code</a>
+        
+      </div>
 
     </div>
   )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
 import ProjectCard from "./ProjectCard"
 import "./Projects.css"
 
 const Projects = () => {
 
-    const colors = ["#0ba95b", "#fc7428", "#7b5ea7", "#12b5e5", "#f9f4da", "#ed203d"]
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://codeguyakash.github.io/server/projects.json").then((res) => setData(res.data))
+    }, [])
+
+   
 
     return (
         <>
             <section id='project-section'>
                 <div id='heading'>
-                <h1>PROJECT</h1>
-                <h3>Our courses are the ultimate brain food</h3>
+                    <h1>PROJECT</h1>
+                    <h3>Our courses are the ultimate brain food</h3>
                 </div>
                 <div className='projects'>
                     {
-                        colors.map((color, index) => (
+                        data.map((color, index) => (
                             <ProjectCard items={color} key={index} />
                         ))
                     }
