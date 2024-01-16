641cf2b0a8521f4814dd6e2241d01e6d99123b5f
Akash <codeguyakash@gmail.com>
2024-01-16 10:26:41 +0530

    add updated resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,42 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <>
-            <div className='container' >
-                <nav>
-                    <div id='logo'>
-                        CODEGUYAKASH
-                    </div>
-                    <div id='nav-items'>
-                        <ul>
-                            <li><a href='#hero-section'>HOME</a></li>
-                            <li><a href='#skill-section'>SKILLS</a></li>
-                            <li><a href='#project-section'>PROJECTS</a></li>
-                            <li><a href='#about-section'>ABOUT</a></li>
-                        </ul>
-                    </div>
-                    <div >
-                        <a id='resume-btn' href="https://drive.google.com/file/d/11EmPEXaep-tYyU7z08I-V5p42Vwc8bGu/view" target='_blank' rel='refer'>DOWNLOAD CV</a>
+  return (
+    <>
+      <div className="container">
+        <nav>
+          <div id="logo">CODEGUYAKASH</div>
+          <div id="nav-items">
+            <ul>
+              <li>
+                <a href="#hero-section">HOME</a>
+              </li>
+              <li>
+                <a href="#skill-section">SKILLS</a>
+              </li>
+              <li>
+                <a href="#project-section">PROJECTS</a>
+              </li>
+              <li>
+                <a href="#about-section">ABOUT</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <a
+              id="resume-btn"
+              href="https://drive.google.com/file/d/1GpLrPq3Fhp76mEkIadNNHXYzv6tR4RTo/view"
+              target="_blank"
+              rel="refer"
+            >
+              DOWNLOAD CV
+            </a>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
 
-                    </div>
-
-                </nav>
-            </div>
-        </>
-    )
-}
-
-export default Navbar
+export default Navbar;
