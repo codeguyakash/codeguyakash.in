2c3aada2e8537dd44c524af243a7c264ca5e17e1
Akash <codeguyakash@gmail.com>
2024-07-07 13:54:37 +0530

    fix all changes

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import "./style.css";
+import node from "../../assets/icons/node-js.svg";
+import mongodb from "../../assets/icons/mongodb.svg";
+import express from "../../assets/icons/express.svg";
+import reactjs from "../../assets/icons/reactjs.svg";
+
+import tailwindcss from "../../assets/icons/tailwindcss.svg";
+import javascript from "../../assets/icons/javascript.svg";
+import html from "../../assets/icons/html.svg";
+import css from "../../assets/icons/css.svg";
+import github from "../../assets/icons/github.svg";
+import mysql from "../../assets/icons/mysql.svg";
 
 const skills = [
-  {
-    name: "MongoDB",
-    icon: "mongodbLogo",
-  },
-  {
-    name: "Express.js",
-    icon: "expressLogo",
-  },
-  {
-    name: "React.js",
-    icon: "react",
-  },
-  {
-    name: "Node.js",
-    icon: "nodejsLogo",
-  },
+  { id: 1, name: "Node", icon: node },
+  { id: 2, name: "MongoDB", icon: mongodb },
+  { id: 3, name: "ExpressJs", icon: express },
+  { id: 4, name: "ReactJs", icon: reactjs },
+
+  { id: 5, name: "Tailwind Css", icon: tailwindcss },
+  { id: 6, name: "JavaScript", icon: javascript },
+  { id: 7, name: "HTML", icon: html },
+  { id: 8, name: "CSS", icon: css },
+  { id: 9, name: "Git/Github", icon: github },
+  { id: 10, name: "MySQL", icon: mysql },
 ];
 
-const index = () => {
+const SkillSection = () => {
   return (
     <section id="skill-section">
       <div id="heading">
@@ -28,15 +34,21 @@ const index = () => {
         <h3>Expertise in technologies</h3>
       </div>
       <div className="container">
-        <div className={`skill skill-${index + 1}`} key={index}>
-          <div className="skill-title">
-            <img src="" alt={`Logo`} className="skills-logos" />
-            <span>"Node"</span>
+        {skills.map((skill, index) => (
+          <div className={`skill skill-${index + 1}`} key={skill.id}>
+            <div className="skill-title">
+              <img
+                src={skill.icon}
+                alt={`${skill.name} Logo`}
+                className="skills-logos"
+              />
+              <span className="color">{skill.name}</span>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
 };
 
-export default index;
+export default SkillSection;
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -17,6 +17,7 @@ const Index = () => {
       setIsOnline(true);
     });
   }, []);
+
   return (
     <>
       <div>
