27e3fc5e74bc6ffe71c17170ddfcb92e1d0368bc
Akash <codeguyakash@gmail.com>
2025-02-13 13:25:53 +0530

    fix

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
-import Cards from "../Cards";
-import axios from "axios";
-import "./style.css";
+import { useState, useEffect } from 'react';
+import Cards from '../Cards';
+import axios from 'axios';
+import './style.css';
 
 const Index = () => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://codeguyakash.github.io/server/data.json")
+      .get('https://codeguyakash.github.io/server/data.json')
       .then((res) => {
         let data = res.data;
         setProjects(Array.isArray(data) ? data : []);
       })
       .catch((e) => {
-        console.error("API Fetch Error:", e.message);
+        console.error('API Fetch Error:', e.message);
         setProjects([]); // Ensure projects is always an array
       });
   }, []);
@@ -23,12 +23,10 @@ const Index = () => {
     <section id="project-section">
       <div id="heading">
         <h1>PROJECT</h1>
-        <h3>Our courses are the ultimate brain food</h3>
       </div>
       <div className="projects">
-        {projects.length > 0 && projects.map((item, index) => (
-          <Cards items={item} key={index} />
-        ))}
+        {projects.length > 0 &&
+          projects.map((item, index) => <Cards items={item} key={index} />)}
       </div>
     </section>
   );
